ce8fd4ab9ecc235b8a081d9b140fec0d9b53f466
Szabolcs Dombi <cprogrammer1994@gmail.com>
2022-12-09 15:11:06 +0200

    add missing include_package_data

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     name='mymodule',
     version='0.1.0',
     ext_modules=[ext],
+    include_package_data=True,
     package_data={'mymodule-stubs': ['__init__.pyi']},
     packages=['mymodule-stubs'],
 )
